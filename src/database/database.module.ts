a0d220beffde6e1e7ebca2c21224232cfef0b303
galla19g <magruesog22@itp.edu.co>
2026-03-04 21:34:09 -0500

    create database

---
+++ src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql', // Change to 'postgres' if using PostgreSQL
+                host: configService.get<string>('DB_HOST', 'localhost'),
+                port: configService.get<number>('DB_PORT', 3306),
+                username: configService.get<string>('DB_USER', 'root'),
+                password: configService.get<string>('DB_PASSWORD', ''),
+                database: configService.get<string>('DB_NAME', 'taller_backend'),
+                autoLoadEntities: true,
+                synchronize: true, // Use only in development
+            }),
+        }),
+    ],
+})
+export class DatabaseModule { }
