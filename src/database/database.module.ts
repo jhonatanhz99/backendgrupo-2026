62600ed93bb30dbeee3ab582190d2d79e1e452d5
galla19g <magruesog22@itp.edu.co>
2026-03-11 19:33:10 -0500

    database update

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -15,7 +15,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
                 password: configService.get<string>('DB_PASSWORD', ''),
                 database: configService.get<string>('DB_NAME', 'taller_backend'),
                 autoLoadEntities: true,
-                synchronize: true, // Use only in development
+                synchronize: true,
             }),
         }),
     ],
